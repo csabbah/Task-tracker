f97c4e17b1fc857050676836ec9c8c977a914fe3
Csabbah <csabbah@ryerson.ca>
2022-03-09 09:20:54 -0500

    Removed button element and JS function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,17 +2,11 @@ var buttonEl = document.querySelector('#save-task');
 var buttonDeleteEl = document.querySelector('#delete-task');
 
 var taskListEl = document.querySelector('.task-list');
-var counter = 0;
 
 var createTask = () => {
   var newTask = document.createElement('li');
   newTask.innerHTML = `<li class="task-item">Task #${counter}</li>`;
   taskListEl.appendChild(newTask);
-  counter++;
 };
 
-var removeTask = () => {
-// };
 buttonEl.addEventListener('click', createTask);
-
-buttonDeleteEl.addEventListener('click', removeTask);
