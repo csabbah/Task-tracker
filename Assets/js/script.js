1b35cf7aea45f8b3e7cf0a67ea24c5dcc7c8db1d
Csabbah <csabbah@ryerson.ca>
2022-03-09 09:44:28 -0500

    Updated code, tasks now get added when hitting add task button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,10 +2,12 @@ var formEl = document.querySelector('#task-form');
 
 var taskListEl = document.querySelector('.task-list');
 
+var counter = 0;
 var createTask = () => {
   var newTask = document.createElement('li');
   newTask.innerHTML = `<li class="task-item">Task #${counter}</li>`;
   taskListEl.appendChild(newTask);
+  counter++;
 };
 
 formEl.addEventListener('submit', (e) => {
