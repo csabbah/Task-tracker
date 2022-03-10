9b4582aa37dd8a78e35c39142bedcc96681c44bf
Csabbah <csabbah@ryerson.ca>
2022-03-09 23:35:52 -0500

    Reverted back to an older version to re-work on functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,44 +1,51 @@
 var formEl = document.querySelector('#task-form');
-var taskListEl = document.querySelector('.task-list');
+var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-// package up data as an object
-var taskDataObj = {};
-
-var taskEl = (taskNameInput, taskTypeInput) => {
-  // Create the LI element
-  var newTask = document.createElement('li');
-  newTask.classList.add('task-info');
-  newTask.innerHTML = `<h4 class="task-item">${taskNameInput}<span class='task-type'>${taskTypeInput}</span></h4>`;
-  // Add it to the main wrapper
-  taskListEl.appendChild(newTask);
-};
-
-var counter = 0;
-var createTask = () => {
+var taskFormHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name'").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
 
-  if (!taskTypeInput || !taskNameInput) {
-    alert('Please fill in the required fields');
-  } else {
-    taskEl(taskNameInput, taskTypeInput);
-
-    // package up data as an object
-    taskDataObj = {
-      id: counter,
-      name: taskNameInput,
-      type: taskTypeInput,
-    };
-    counter++;
-
-    // Reset value of input
-    // Forms have a reset option to clear all fields
-    formEl.reset();
+  // check if inputs are empty (validate)
+  if (taskNameInput === '' || taskTypeInput === '') {
+    alert('You need to fill out the task form!');
+    return false;
   }
+
+  formEl.reset();
+
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = '';
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
+
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+
+  createTaskEl(taskDataObj);
 };
 
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault();
+var createTaskEl = function (taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement('li');
+  listItemEl.className = 'task-item';
 
-  createTask();
-});
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement('div');
+  taskInfoEl.className = 'task-info';
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
+    "</h3><span class='task-type'>" +
+    taskDataObj.type +
+    '</span>';
+  listItemEl.appendChild(taskInfoEl);
+
+  console.dir(listItemEl);
+
+  // add list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener('submit', taskFormHandler);
