955b0b2239dff42e570a73422ba0c1c56375db48
Csabbah <csabbah@ryerson.ca>
2022-03-10 12:22:48 -0500

    Added comments in the JS file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,34 @@
+// #task-form is the form itself, we use an event listener on it and we
+// use the formEl.reset() function to clear the input after submit
 var formEl = document.querySelector('#task-form');
+// This is the UL element we want to add our LI in
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 var taskFormHandler = function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Don't refresh browser upon form submit
+
+  // Extract the data from the input and select elements
+  // Select being the drop down element, name ='task-type' being the options in the dropddwn
   var taskNameInput = document.querySelector("input[name='task-name'").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  // check if inputs are empty (validate)
+  // Check if inputs are empty (validate)
   if (!taskNameInput || !taskTypeInput) {
     alert('You need to fill out the task form!');
     return false;
   }
 
+  // Clear the form (reset everything to blank)
   formEl.reset();
 
+  // Create a object to hold all the data the user inputs
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput,
   };
 
+  // Pass down the object we created and updated into into the function
+  // Function builds the actual element based on the data we receive
   createTaskEl(taskDataObj);
 };
 
@@ -28,7 +38,11 @@ var createTaskEl = function (taskDataObj) {
   listItemEl.className = 'task-item';
 
   // create div to hold task info and add to list item
-  listItemEl.innerHTML = `<div class="task-info"><h3 class="task-name">${taskDataObj.name}</h3><span class="task-type">${taskDataObj.type}</span></div>`;
+  listItemEl.innerHTML = `
+  <div class="task-info">
+  <h3 class="task-name">${taskDataObj.name}</h3>
+  <span class="task-type">${taskDataObj.type}</span>
+  </div>`;
 
   // add list item to list
   tasksToDoEl.appendChild(listItemEl);
